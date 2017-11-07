0f55b834b22d4e37e966463a4856a759c6500c06
Claire Phillips <claireannphillips15@gmail.com>
2017-11-07 03:52:55 +0000

    finished testing

--- memos/testing.py
+++ memos/testing.py
@@ -1,25 +1,4 @@
-
-import flask
-from flask import g
-from flask import render_template
-from flask import request
-from flask import url_for
-
-from bson.objectid import ObjectId
-
-import json
-import logging
-
-import sys
-
-# Date handling
-import arrow
-from dateutil import tz  # For interpreting local times
-
-# Mongo database
-from pymongo import MongoClient
-
-import configfrom flask_main import *
+from flask_main import *
 import arrow 
 
 import nose
@@ -30,19 +9,40 @@ def test_regular():
 	'''
 	Tests creating certain memos
 	'''
-    memo = "this is my memo"
-    date = "2016-11-01"
-    assert(create_helper(memo, date)["text"] == memo)
-    assert(create_helper(memo, date)["date"] == arrow.get(date).replace(tzinfo = tz.tzlocal()).isoformat())
+    db.collections.insertOne(
+    {
+    "_id": {
+        "$oid": "5a012b6674fece54caaa988b"
+    },
+    "text": "Do Laundry",
+    "date": "2016-02-10"
+    }
+    assert get_memos("Do Laundry") == "_id": {"$oid": "5a012b6674fece54caaa988b"}
     
     
-    return None 
     
+    db.collections.insertOne(
+  {
+    "_id": {
+        "$oid": "5a012d5474fece634399a5f8"
+    },
+    "text": "new years",
+    "date": "2015-01-01"
+}
+    assert get_memos("new years") == "_id": {"$oid": "5a012d5474fece634399a5f8"}
+   
+   
+   {
+    "_id": {
+        "$oid": "5a012daa74fece634399a5f9"
+    },
     
+    "text": "christmas",
+    "date": "2005-02-12"
+}
     
-    
-    
-    
+    assert get_memos("christmas") == "_id": {"$oid": "5a012daa74fece634399a5f9"}
+
     
     
     
